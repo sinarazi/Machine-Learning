76cb8b8308844248cbf7755d2b111881d076fc9f
sinarazi <razisina78@gmail.com>
2022-03-27 00:13:46 +0430

    big improvement

---
+++ logistic regression/utils.py
@@ -0,0 +1,62 @@
+import sys
+import numpy as np
+from matplotlib import pyplot
+
+sys.path.append('..')
+
+
+def mapFeature(X1, X2, degree=6):
+
+    if X1.ndim > 0:
+        out = [np.ones(X1.shape[0])]
+    else:
+        out = [np.ones(1)]
+
+    for i in range(1, degree + 1):
+        for j in range(i + 1):
+            out.append((X1 ** (i - j)) * (X2 ** j))
+
+    if X1.ndim > 0:
+        return np.stack(out, axis=1)
+    else:
+        return np.array(out)
+
+
+def plotDecisionBoundary(plotData, theta, X, y):
+    # make sure theta is a numpy array
+    theta = np.array(theta)
+
+    # Plot Data (remember first column in X is the intercept)
+    plotData(X[:, 1:3], y)
+
+    if X.shape[1] <= 3:
+        # Only need 2 points to define a line, so choose two endpoints
+        plot_x = np.array([np.min(X[:, 1]) - 2, np.max(X[:, 1]) + 2])
+
+        # Calculate the decision boundary line
+        plot_y = (-1. / theta[2]) * (theta[1] * plot_x + theta[0]) if theta[2] != 0 else 0
+
+        # Plot, and adjust axes for better viewing
+        pyplot.plot(plot_x, plot_y)
+
+        # Legend, specific for the exercise
+        pyplot.legend(['Admitted', 'Not admitted', 'Decision Boundary'])
+        pyplot.xlim([30, 100])
+        pyplot.ylim([30, 100])
+    else:
+        # Here is the grid range
+        u = np.linspace(-1, 1.5, 50)
+        v = np.linspace(-1, 1.5, 50)
+
+        z = np.zeros((u.size, v.size))
+        # Evaluate z = theta*x over the grid
+        for i, ui in enumerate(u):
+            for j, vj in enumerate(v):
+                z[i, j] = np.dot(mapFeature(ui, vj), theta)
+
+        z = z.T  # important to transpose z before calling contour
+        # print(z)
+
+        # Plot z = 0
+        pyplot.contour(u, v, z, levels=[0], linewidths=2, colors='g')
+        pyplot.contourf(u, v, z, levels=[np.min(z), 0, np.max(z)], cmap='Greens', alpha=0.4)
